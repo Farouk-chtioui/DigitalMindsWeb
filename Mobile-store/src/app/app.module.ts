0ccea99cfc2d8f4cd235b246054f07a567c34298
farouk-chtioui <chtiouiaa25@gmail.com>
2024-02-03 23:21:05 +0100

    here

--- Mobile-store/src/app/app.module.ts
+++ Mobile-store/src/app/app.module.ts
@@ -4,13 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
-import { MiddleComponent } from './middle/middle.component';
+import { HomeComponent } from './home/home.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    MiddleComponent
+    HomeComponent,
+    ProductListComponent,
+    
   ],
   imports: [
     BrowserModule,

d76c19bf7ea4262efc5ffc37d7ca13388b94a3c6
farouk-chtioui <chtiouiaa25@gmail.com>
2024-02-04 01:24:39 +0100

    t

--- Mobile-store/src/app/app.module.ts
+++ Mobile-store/src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HomeComponent } from './home/home.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
@@ -13,11 +16,12 @@ import { ProductListComponent } from './product-list/product-list.component';
     NavBarComponent,
     HomeComponent,
     ProductListComponent,
+    RegisterComponent,
     
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,ReactiveFormsModule,HttpClientModule,FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
